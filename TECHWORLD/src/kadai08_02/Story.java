07a181c5f97b1af779f5fc5a31cd452b9b1bb6c0
aoki_shou <aoki.shou@it-sp.co.jp>
2024-08-02 00:41:01 +0900

    kadai08_01とkadai08_02を追加

---
+++ TECHWORLD/src/kadai08_02/Story.java
@@ -0,0 +1,38 @@
+package kadai08_02;
+
+import java.util.Scanner;
+
+public class Story {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		Hero h = new Hero();
+		System.out.println("勇者が誕生した");
+		System.out.println("名前:" + h.name);
+		System.out.println("体力:" + h.hp);
+		System.out.println("攻撃力:" + h.at);
+		System.out.println("防御力:" + h.df);
+
+		Scanner scan2 = new Scanner(System.in);
+		monster m = new monster();
+		System.out.println("モンスターが誕生した");
+		System.out.println("名前:" + m.name);
+		System.out.println("体力:" + m.hp);
+		System.out.println("攻撃力:" + m.at);
+		System.out.println("防御力:" + m.df);
+
+		Scanner Mscan = new Scanner(System.in);
+		magician mag = new magician();
+		System.out.println("魔法使いが誕生した");
+		System.out.println("名前:" + mag.name);
+		System.out.println("体力:" + mag.hp);
+		System.out.println("攻撃力:" + mag.mp);
+
+		scan.close();
+		scan2.close();
+		Mscan.close();
+	}
+
+}
---
+++ TECHWORLD/src/kadai08_02/monster.java
@@ -0,0 +1,20 @@
+package kadai08_02;
+
+public class monster {
+	// フィールド
+	String name; // 名前
+	int hp; // HP 
+	int at; // 攻撃力
+	int df; // 防御力
+
+	void atack() {
+		System.out.println(this.at+"の攻撃");
+		System.out.println(this.at+"のダメージ");
+	}
+	
+	void defense() {
+		System.out.println(this.name+"は守りを固めた");
+		this.df = this.df+10;
+		System.out.println("防御力が10上がった");
+	}
+}
