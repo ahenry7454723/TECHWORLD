73090ecb17561d6441240df277303c5b7577fd61
aoki_shou <aoki.shou@it-sp.co.jp>
2024-08-15 23:10:18 +0900

    プッシュ

---
+++ TECHWORLD/src/kadai10_01/EvolvedPokemon.java
@@ -0,0 +1,9 @@
+package kadai10_01;
+
+public class EvolvedPokemon extends Pokemon{
+	public int Wattack;
+	public int hear;
+	
+	
+
+}
---
+++ TECHWORLD/src/kadai10_01/Pokemon.java
@@ -0,0 +1,14 @@
+package kadai10_01;
+
+public class Pokemon {
+	public int Attack;
+	public int Defence;
+	
+	public void PokemonAttack() {
+		System.out.println("攻撃した");
+	}
+	
+	public void PokemonDefence() {
+		System.out.println("防御した");
+	}
+}
